7bd4168ae296a78cf754cafe2c0c10d45eefb040
adisingh-dev <adityasingh.devx@gmail.com>
2025-02-10 01:01:37 +0530

    create sample SSE service for practice

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './');
+app.use(express.static('./'));
+
+app.get('/', (req, res) => {
+    res.status(200).render('index');
+});
+
+let clients = [];
+app.get('/events', (req, res) => {
+    res.set({
+        'Content-Type': 'text/event-stream',
+        'Connection': 'keep-alive',
+        'Cache-Control': 'no-cache'
+    });
+
+    clients.push({res, req});
+});
+
+app.post('/events', (req, res) => {
+    clients[0].res.write('data: SSE service triggered\n\n');
+
+    let counter = 1;
+    const workload = setInterval(() => {
+        if(counter > 5) {
+            clearInterval(workload);
+            clients[0].res.write('event: close\n'); 
+            clients[0].res.write('data: SSE service closed\n\n');
+            clients[0].res.on('close', () => {
+                clients[0].res.end();
+            });
+            clients[0].req.on('close', () => {
+                clients[0].res.end();
+            });
+            return res.status(200).json({
+                status: 200,
+                message: "user created successfully"
+            });
+
+        } else {
+            clients[0].res.write(`data: Message Number ${counter}\n\n`);
+            counter++;
+        }
+
+    }, 2000);
+});
+
+app.listen(4000, () => {
+    console.log('server running @3000');
+});
